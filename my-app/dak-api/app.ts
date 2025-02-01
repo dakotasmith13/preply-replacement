352374899e8a8975e2b754372c988f49b459a499
Dakota Smith <dakota.13.smith@gmail.com>
2025-02-01 16:24:05 -0500

    api app.ts

---
+++ my-app/dak-api/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import authRouter from './auth/routes';
+import lessonRouter from './lessons/routes';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/login', authRouter);
+app.use('/lessons', lessonRouter);
+
+app.listen(3030, () => {
+	console.log('Server running on port 3030');
+});
