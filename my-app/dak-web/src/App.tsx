8bb4bead8d20293124491d8ab250b77aa0ea5056
Dakota Smith <dakota.13.smith@gmail.com>
2025-07-13 22:17:00 -0400

    Add tanstack query to app router, add more routes

--- my-app/dak-web/src/App.tsx
+++ my-app/dak-web/src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Nav from './components/Nav/Nav';
+import { LessonPage } from './components/Lesson/LessonPage';
+import { ScheduleLesson } from './components/Schedule/ScheduleLesson';
+import { CreateAccount } from './components/Login/CreateAccount';
+
+const queryClient = new QueryClient();
 
 const App = () => {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/home" element={<Home />} />
-			</Routes>
-		</Router>
+		<QueryClientProvider client={queryClient}>
+			<Router>
+				<div>
+					<Nav />
+				</div>
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/lessons" element={<LessonPage />} />
+					<Route path="/schedule" element={<ScheduleLesson />} />
+					<Route path="/create-account" element={<CreateAccount />} />
+				</Routes>
+			</Router>
+		</QueryClientProvider>
 	);
 };
 
