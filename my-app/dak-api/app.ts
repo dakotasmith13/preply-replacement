a5b4e518b908b0ea2862365c6bd3ace9a79eab49
Dakota Smith <dakota.13.smith@gmail.com>
2025-07-13 22:22:09 -0400

    add user router to express app

--- my-app/dak-api/app.ts
+++ my-app/dak-api/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRouter from './auth/routes';
 import lessonRouter from './lessons/routes';
+import userRouter from './users/routes';
 import cors from 'cors';
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/login', authRouter);
 app.use('/lessons', lessonRouter);
+app.use('/users', userRouter);
 
 app.listen(3030, () => {
 	console.log('Server running on port 3030');
